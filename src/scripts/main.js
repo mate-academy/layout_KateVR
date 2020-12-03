b6ff242543672526e56fd7ce60a30d41f9beee55
Olesia <olesia.tk@gmail.com>
2020-12-03 15:18:45 +0200

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+      // const menu = document.querySelector('.nav');
+      // const toggler = document.querySelector('.nav__link');
+      // toggler.onclick = function toggleSidebar() {
+      //   menu.classList.toggle('nav--open');
+      //   toggler.classList.toggle('nav__link--active');
+      // };
