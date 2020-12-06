7f987e84bd198b11ddc77ae7a7be3c0bc5718bbc
Your Name <mogolgogol.90@gmail.com>
2020-12-07 00:40:28 +0200

    hided button on scroll, added tech description on hover

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -30,3 +30,26 @@ $(document).ready(function() {
     pagerSelector: '.about__controls-points',
   });
 });
+
+window.addEventListener('scroll', findScroll);
+
+const button = document.querySelector('.header__button');
+const buttonOverlay = document.querySelector('.header__button-overlay');
+
+function findScroll() {
+  // eslint-disable-next-line max-len
+  const windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  // eslint-disable-next-line max-len
+  const windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const contactSection = document.querySelector('.contact');
+  const contactHeight = contactSection.offsetHeight;
+  const heightWOContacts = windowHeight - contactHeight;
+
+  if (heightWOContacts < windowScroll) {
+    button.style.display = 'none';
+    buttonOverlay.style.display = 'none';
+  } else {
+    button.style.display = 'inline-flex';
+    buttonOverlay.style.display = 'block';
+  }
+}
