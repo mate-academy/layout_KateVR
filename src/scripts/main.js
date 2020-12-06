547dea3ceaa9aea4f8f4332a57edde7837402cf1
Your Name <mogolgogol.90@gmail.com>
2020-12-06 15:50:53 +0200

    solution reinstalled

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,32 @@
 'use strict';
+
+/* eslint-env jquery */
+
+$(document).ready(function() {
+  $('.home__image-container').bxSlider({
+    slideSelector: '.home__prod-wrap',
+    wrapperClass: 'header__slider',
+    nextSelector: '.home__next',
+    prevSelector: '.home__prev',
+    nextText: '',
+    prevText: '',
+    pager: false,
+    auto: true,
+    maxSlides: 1,
+    shrinkItems: true,
+  });
+
+  $('.about__slider').bxSlider({
+    slideSelector: '.about__slide',
+    wrapperClass: 'about__slider',
+    nextSelector: '.about__next',
+    prevSelector: '.about__prev',
+    nextText: '',
+    prevText: '',
+    pager: true,
+    auto: true,
+    maxSlides: 1,
+    shrinkItems: true,
+    pagerSelector: '.about__controls-points',
+  });
+});
