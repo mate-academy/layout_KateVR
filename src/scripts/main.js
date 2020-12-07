7ea2b35f14b4f3bb1cb317d760e4aea80189358e
Alex <alexmogwaiii@gmail.com>
2020-12-07 23:02:27 +0200

    added two blocks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const burger = document.querySelector('.nav__burger-container');
+const burgerMenu = document.querySelector('.nav__menu-bar');
+
+burger.addEventListener('click', function() {
+  this.classList.toggle('nav__burger-container--active');
+  burgerMenu.classList.toggle('nav__menu-bar--active');
+});
