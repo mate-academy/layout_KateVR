a7367c0651f8ada44eabcc92a09efca3b58a97b7
Igor Stasiv <igorstsv@gmail.com>
2021-04-14 09:49:18 +0300

    Header adaptive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const nav = document.querySelector('.nav-mobile');
+const toggler = document.querySelector('#toggler');
+
+toggler.addEventListener('click', () => {
+  nav.classList.toggle('nav-mobile--active');
+  toggler.classList.toggle('top-actions__nav-mobile-toggler--active');
+});
