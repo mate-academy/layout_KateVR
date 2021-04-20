2390f9ad2886c42d49e973ebcf8720a5fb77c3f4
amithos <alex.theutos@gmail.com>
2021-04-20 15:20:01 +0300

    tablet version was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,5 +3,44 @@
 const $ = window.$;
 
 $(document).ready(function() {
-  // console.log('jQuery is connected successfully');
+  const menuBtn = $('.header__menu-btn');
+  const menu = $('.menu');
+  const menuToggler = 'menu--switch';
+  const menuBtnToggler = 'header__menu-btn--showed';
+
+  const toggleMenu = () => {
+    menu.toggleClass(menuToggler);
+    menuBtn.toggleClass(menuBtnToggler);
+  };
+
+  menuBtn.click(toggleMenu);
+
+  const specMain = $('.specifications__main');
+  const senBtn = $('#sen');
+  const batBtn = $('#bat');
+  const conBtn = $('#con');
+
+  const openSenClass = 'specifications__main--sen';
+  const openBatClass = 'specifications__main--bat';
+  const openConClass = 'specifications__main--con';
+  const buttonChange = 'specifications__button--open';
+
+  const senSwitcher = () => {
+    specMain.toggleClass(openSenClass);
+    senBtn.toggleClass(buttonChange);
+  };
+
+  const batSwitcher = () => {
+    specMain.toggleClass(openBatClass);
+    batBtn.toggleClass(buttonChange);
+  };
+
+  const conSwitcher = () => {
+    specMain.toggleClass(openConClass);
+    conBtn.toggleClass(buttonChange);
+  };
+
+  senBtn.click(senSwitcher);
+  batBtn.click(batSwitcher);
+  conBtn.click(conSwitcher);
 });
