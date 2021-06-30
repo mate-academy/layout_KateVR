8100d40b2c1f5d40b7d5f2b25c1307ffaa4ccc4d
Yaroslav <gso218695@gmail.com>
2021-06-30 18:31:15 +0300

    test2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const nav = document.querySelector('.nav-mobile');
+const toggler = document.getElementById('toggler');
+
+toggler.addEventListener('click', () => {
+  nav.classList.toggle('nav-mobile--active');
+  toggler.classList.toggle('top-actions__nav-mobile-toggler--active');
+});
