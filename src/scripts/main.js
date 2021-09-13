cd31b6d7debd2831b365ebe1bfd08c650fbb8c68
Oleksandr Krasnoshchokov <krasnoshchokovoleksandr@gmail.com>
2021-09-13 12:43:55 +0300

    90% finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const pageBody = document.querySelector('.page__body');
+const menuOpen = document.querySelector('.header__menu--opener');
+const menuCloser = document.querySelector('.menuByList--item');
+const lenguageOpen = document.querySelector('.goto-language');
+const lenguageCloser = document.querySelector('.menuLanguage');
+// const header = document.querySelector('.header');
+// const mainContant = document.querySelector('.main');
+// const footer = document.querySelector('.footer');
+
+menuOpen.addEventListener('click', function() {
+  pageBody.classList.add('hidden-blocks');
+});
+
+menuCloser.addEventListener('click', function() {
+  pageBody.classList.remove('hidden-blocks');
+});
+
+lenguageOpen.addEventListener('click', function() {
+  pageBody.classList.add('hidden-blocks');
+});
+
+lenguageCloser.addEventListener('click', function() {
+  pageBody.classList.remove('hidden-blocks');
+});
