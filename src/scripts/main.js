b23028277c6aa0b5f57193809f4936baef8f43e3
Oleksandr Krasnoshchokov <krasnoshchokovoleksandr@gmail.com>
2021-09-14 17:22:44 +0300

    95% done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,49 @@
 'use strict';
 
 const pageBody = document.querySelector('.page__body');
-const menuOpen = document.querySelector('.header__menu');
+// const menuOpen = document.querySelector('.header__menu');
 const menuCloser = document.querySelector('.menuByList--item');
 const lenguageOpen = document.querySelector('.goto-language');
 const lenguageCloser = document.querySelector('.menuLanguage');
 const menuOrder = document.querySelector('.menuOrder__closer');
-// const mainContant = document.querySelector('.main');
-// const footer = document.querySelector('.footer');
 
-menuOpen.addEventListener('click', function() {
-  pageBody.classList.add('hidden-blocks');
+function visibleOff(find, page) {
+  find.addEventListener('click', function() {
+    page.classList.add('hiddenOn');
+  });
+}
+
+function visibleOn(find, page) {
+  find.addEventListener('click', function() {
+    page.classList.remove('hiddenOn');
+  });
+}
+
+const completeOrder = document.querySelector('.menuOrder__button');
+const orderForm = document.querySelector('.grid__menuOrder--order');
+const completeOrderOn = document.querySelector('.menuOrder__complete');
+
+completeOrder.addEventListener('click', function() {
+  completeOrderOn.classList.add('hiddenOn');
+});
+
+completeOrder.addEventListener('click', function() {
+  orderForm.classList.add('hiddenOff');
+});
+
+const findPage = document.querySelector('.menuOrder__quantity-icon');
+const pageAction = document.querySelector('.menuOrder__quantityMenu');
+const dropNum = document.querySelector('.menuOrder__quantity-number');
+
+visibleOn(findPage, pageAction);
+visibleOff(findPage, pageAction);
+
+findPage.addEventListener('click', function() {
+  pageAction.classList.add('hiddenOn');
+});
+
+dropNum.addEventListener('click', function() {
+  pageAction.classList.remove('hiddenOn');
 });
 
 menuCloser.addEventListener('click', function() {
