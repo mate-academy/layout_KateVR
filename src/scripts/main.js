e22be2b39504a49c4631bbd1f9bb5c9788f3c1d5
Oleksandr Krasnoshchokov <krasnoshchokovoleksandr@gmail.com>
2021-09-20 18:51:27 +0300

    Fixed remarks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,7 @@
 'use strict';
 
-// const pageBody = document.querySelector('.page__body');
-// // const menuOpen = document.querySelector('.header__menu');
-// const menuCloser = document.querySelector('.menuByList--item');
-// const lenguageOpen = document.querySelector('.goto-language');
-// const lenguageCloser = document.querySelector('.menuLanguage');
-// const menuOrder = document.querySelector('.menuOrder__closer');
+// find page body
+const pageBody = document.querySelector('.page__body');
 
 function addDisplayNone(find, page) {
   find.addEventListener('click', function() {
@@ -43,6 +39,34 @@ function turnOnScroll(find, page) {
   });
 }
 
+/** Work with Main Info */
+
+// find menu opener
+const menuOpener = document.querySelector('.header__menu--opener');
+const faqOpener = document.querySelector('.headerFAQ--opener');
+const helpOpener = document.querySelector('.headerHelp--opener');
+
+// disable scroll on body  menuByList--closer
+turnOffScroll(menuOpener, pageBody);
+turnOffScroll(faqOpener, pageBody);
+turnOffScroll(helpOpener, pageBody);
+
+// find menu: list, language, help and FAQ closer
+const menuListCloser = document.querySelector('.menuByList--closer');
+const menuLanguageCloser = document.querySelector('.menuLanguage--closer');
+const menuFAQCloser = document.querySelector('.helpFAQ--closer');
+const menuHelpCloser = document.querySelector('.helpFAQ--closer1');
+const menuHelpToContacts = document.querySelector('.helpFAQ__link--contacts');
+
+// turn on scroll on body
+turnOnScroll(menuListCloser, pageBody);
+turnOnScroll(menuLanguageCloser, pageBody);
+turnOnScroll(menuFAQCloser, pageBody);
+turnOnScroll(menuHelpCloser, pageBody);
+turnOnScroll(menuHelpToContacts, pageBody);
+
+/** End Work with Main Info */
+
 /** Work with Tech Info */
 
 // find focus bottom
@@ -71,10 +95,6 @@ removeDisplayBlock(connectClosure, connectInfo);
 
 /** Work with Order menu */
 
-// find page body
-const pageBody = document.querySelector('.page__body');
-// find order menu
-// const orderMenu = document.querySelector('.grid__menuOrder--order');
 // find order form
 const orderForm = document.querySelector('.grid__menuOrder--order');
 // find pay form
@@ -93,64 +113,35 @@ turnOffScroll(clickBuyButton1, pageBody);
 turnOffScroll(clickBuyButton2, pageBody);
 
 // close order menu
-const closeIconClick = document.querySelector('.menuOrder__closer');
+const orderCloser = document.querySelector('.menuOrder__closer');
 
-turnOnScroll(closeIconClick, pageBody); // turn on scroll on body
-removeDisplayNone(closeIconClick, orderForm);
-// removeDisplayBlock(closeIconClick, completeOrder);
+turnOnScroll(orderCloser, pageBody); // turn on scroll on body
+removeDisplayNone(orderCloser, orderForm);
+removeDisplayBlock(orderCloser, completePage);
 
 // click to purchase button
 const clickOrderButton = document.querySelector('.menuOrder__button');
+const navOrder = document.querySelector('.menuOrder__linkOrder');
+const navComplete = document.querySelector('.menuOrder__linkComplete');
 
+turnOffScroll(clickOrderButton, pageBody); // disable scroll on body
 addDisplayNone(clickOrderButton, orderForm); // close order form
 addDisplayBlock(clickOrderButton, completePage); // open complete message
 
-// click to complete button
+orderCloser.addEventListener('click', function() {
+  navOrder.classList.add('menuOrder__link--active');
+});
+
+clickOrderButton.addEventListener('click', function() {
+  navOrder.classList.remove('menuOrder__link--active');
+});
+
+clickOrderButton.addEventListener('click', function() {
+  navComplete.classList.add('menuOrder__link--active');
+});
+
+orderCloser.addEventListener('click', function() {
+  navComplete.classList.remove('menuOrder__link--active');
+});
 
 /* */
-
-// const completeOrder = document.querySelector('.menuOrder__button');
-// const orderForm = document.querySelector('.grid__menuOrder--order');
-// const completeOrderOn = document.querySelector('.menuOrder__complete');
-// const closeIconClick = document.querySelector('.menuOrder__closer');
-
-// addDisplayBlock(closeIconClick, orderForm);
-
-// completeOrder.addEventListener('click', function() {
-//   completeOrderOn.classList.add('hiddenOn');
-// });
-
-// completeOrder.addEventListener('click', function() {
-//   orderForm.classList.add('hiddenOff');
-// });
-
-// const findPage = document.querySelector('.menuOrder__quantity-icon');
-// const pageAction = document.querySelector('.menuOrder__quantityMenu');
-// const dropNum = document.querySelector('.menuOrder__quantity-number');
-
-// visibleOn(findPage, pageAction);
-// visibleOff(findPage, pageAction);
-
-// findPage.addEventListener('click', function() {
-//   pageAction.classList.add('hiddenOn');
-// });
-
-// dropNum.addEventListener('click', function() {
-//   pageAction.classList.remove('hiddenOn');
-// });
-
-// menuCloser.addEventListener('click', function() {
-//   pageBody.classList.remove('hidden-blocks');
-// });
-
-// lenguageOpen.addEventListener('click', function() {
-//   pageBody.classList.add('hidden-blocks');
-// });
-
-// lenguageCloser.addEventListener('click', function() {
-//   pageBody.classList.remove('hidden-blocks');
-// });
-
-// menuOrder.addEventListener('click', function() {
-//   pageBody.classList.remove('hidden-blocks');
-// });
