3051da880405572c1a6ec94a6fdb217c71ae4db5
Samson Sim <samson.sim@ukr.net>
2021-12-07 11:19:29 +0200

    add button

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,17 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.querySelector('.contact-us__form');
+const inputs = document.querySelectorAll('.contact-us__form-input');
+const textarea = document.querySelector('.contact-us__form-text');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  inputs.forEach((elem) => {
+    elem.value = '';
+  });
+
+  textarea.value = '';
+});
