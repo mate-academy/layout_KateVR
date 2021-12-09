db1c9165b0da80a34f99c874864ac8012b7d3043
Alona Snigur <alonaatamanenkoo>
2021-12-09 19:47:57 +0200

    mobilev

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('.contacts-us__form');
+const inputs = document.querySelectorAll('.contacts-us__form-input');
+const textarea = document.querySelector('.contacts-us__form-text');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+
+  textarea.value = '';
+});
