4f24490e88450b57dba263f8582ebc7809b1e342
Heorhii Plugoyi <gladiator943work@gmail.com>
2022-01-16 19:53:18 +0200

    save 1/16/2222 7:22pm

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const answer = document.querySelector('.question-answer-block__answer');
+
+const toggler1 = document.querySelector('#menu-faq-1');
+
+toggler1.addEventListener('click', () => {
+  answer.classList.toggle('question-answer-block__answer--hidden-1');
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+
+  if (window.location.hash === '#menu-language') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+
+  if (window.location.hash === '#menu-help') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+
+  if (window.location.hash === '#menu-faq') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
