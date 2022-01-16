07648cc0ccc2777f0114c77a17a830f5953afc39
trtskvalerie <trtsk.tea.valerie@gmail.com>
2022-01-16 19:35:23 +0200

    adapted Contact Us section for tablet and desktop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+document.querySelector('#contact').addEventListener('submit',
+  function(event) {
+    event.preventDefault();
+
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('tel').value = '';
+    document.getElementById('message').value = '';
+  }
+);
