7c2c4576def3930f2970585d43138d765d163b95
trtskvalerie <trtsk.tea.valerie@gmail.com>
2022-01-17 15:59:32 +0200

    added menu and language menu for mob and tablet

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,16 @@
 'use strict';
 
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu'
+    || window.location.hash === '#lang'
+    || window.location.hash === '#faq'
+    || window.location.hash === '#help') {
+    document.body.classList.add('page__body--window-active');
+  } else {
+    document.body.classList.remove('page__body--window-active');
+  }
+});
+
 document.querySelector('#contact').addEventListener('submit',
   function(event) {
     event.preventDefault();
