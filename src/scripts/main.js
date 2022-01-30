98d14d572f08f72c1c11bb9a37da7f4f0f2c81ce
Dorosh13 <dorosh.d.13@gmail.com>
2022-01-30 18:55:44 +0200

    progress 88%

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const open = document.querySelector('.faq__question-question');
+const popup = document.querySelector('.faq__question-block');
+
+open.addEventListener('click', function() {
+  if (popup.style.display === 'none') {
+    popup.style.display = 'flex';
+  } else {
+    popup.style.display = 'none';
+  }
+});
