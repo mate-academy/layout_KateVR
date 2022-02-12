21a45cc862dfe85f179147815c3b6e981bf8187c
Heorhii Plugoyi <gladiator943work@gmail.com>
2022-02-12 13:22:12 +0200

    save 2/12/2222

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -40,3 +40,24 @@ document.querySelector('#consult').addEventListener('submit',
     document.getElementById('message').value = '';
   }
 );
+
+const button1 = document.querySelector('#circle-button--1');
+const button2 = document.querySelector('#circle-button--2');
+const button3 = document.querySelector('#circle-button--3');
+
+const button1hidden = document
+  .querySelector('.main-imgs__icon--tech-specs-hidden');
+const button2hidden = document.querySelector('.tech-specs__text--hidden2');
+const button3hidden = document.querySelector('.tech-specs__text--hidden3');
+
+button1.addEventListener('click', () => {
+  button1hidden.classList.toggle('.main-imgs__icon--tech-specs-hidden');
+});
+
+button2.addEventListener('click', () => {
+  button2hidden.classList.toggle('.tech-specs__text--un-hidden2');
+});
+
+button3.addEventListener('click', () => {
+  button3hidden.classList.toggle('.tech-specs__text--un-hidden3');
+});
