6bd49417dcdeb5b897155aa3cd2459ee51c0ad73
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-04-19 18:05:41 +0300

    +video section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -41,4 +41,56 @@ item7.addEventListener('click', () => {
   document.getElementById('menu-container').classList.toggle('menu-opened');
 });
 
-document.getElementById('message').value = '';
+const prev = document.getElementById('headerLink1');
+const next = document.getElementById('headerLink2');
+
+const prev1 = document.getElementById('productLink1');
+const next1 = document.getElementById('productLink2');
+
+prev.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  next.classList.remove('header__link--active');
+  prev.classList.add('header__link--active');
+});
+
+next.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  prev.classList.remove('header__link--active');
+  next.classList.add('header__link--active');
+});
+
+prev1.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  next1.classList.remove('product__link--active');
+  prev1.classList.add('product__link--active');
+
+  document.getElementById('player').classList.add('product__video--active');
+  document.getElementById('player1').classList.remove('product__video--active');
+
+  document.getElementById('button').classList.add('product__button--active');
+
+  const button1 = document.getElementById('button1');
+
+  button1.classList.remove('product__button--active');
+
+  document.getElementById('count').textContent = '1/2';
+});
+
+next1.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  prev1.classList.remove('product__link--active');
+  next1.classList.add('product__link--active');
+
+  document.getElementById('player').classList.remove('product__video--active');
+  document.getElementById('player1').classList.add('product__video--active');
+
+  document.getElementById('button').classList.remove('product__button--active');
+
+  document.getElementById('button1').classList.add('product__button--active');
+
+  document.getElementById('count').textContent = '2/2';
+});
