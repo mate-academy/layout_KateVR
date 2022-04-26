a78dffcb0b4a3ba8dc9527bc1cbf228d91a727ea
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-04-26 23:37:39 +0300

    fix

--- src/scripts/checkout.js
+++ src/scripts/checkout.js
@@ -1,86 +1,86 @@
-'use strict';
+// 'use strict';
 
-// function nameInput() {
-//   if (!document.getElementById('name').value.match(/[0-9]/)) {
-//     return true;
+// // function nameInput() {
+// //   if (!document.getElementById('name').value.match(/[0-9]/)) {
+// //     return true;
+// //   }
+// // }
+
+// // function phoneInput() {
+// //   if (!document.getElementById('phone').value.match(/[a-zA-Z]/)) {
+// //     return true;
+// //   }
+// // }
+
+// // const input1 = document.getElementById('input1');
+// // const text1 = document.getElementById('invalid1');
+
+// // input1.addEventListener('blur', (e) => {
+// //   e.preventDefault();
+
+// //   if (nameInput(input1)) {
+// //     input1.classList.remove('checkout__input--invalid');
+// //     input1.classList.add('checkout__input--valid');
+// //     text1.classList.remove('checkout__invalid--active');
+// //   }
+// // });
+
+// // input1.addEventListener('blur', (e) => {
+// //   e.preventDefault();
+
+// //   if (!nameInput(input1)) {
+// //     input1.classList.remove('checkout__input--valid');
+// //     input1.classList.add('checkout__input--invalid');
+// //     text1.classList.add('checkout__invalid--active');
+// //   }
+// // });
+
+// // const input4 = document.getElementById('input4');
+// // const text4 = document.getElementById('invalid4');
+
+// // input4.addEventListener('blur', (e) => {
+// //   e.preventDefault();
+
+// //   if (phoneInput(input4)) {
+// //     input4.classList.remove('checkout__input--invalid');
+// //     input4.classList.add('checkout__input--valid');
+// //     text4.classList.remove('checkout__invalid--active');
+// //   }
+// // });
+
+// // input4.addEventListener('blur', (e) => {
+// //   e.preventDefault();
+
+// //   if (!nameInput(input4)) {
+// //     input4.classList.remove('checkout__input--valid');
+// //     input4.classList.add('checkout__input--invalid');
+// //     text4.classList.add('checkout__invalid--active');
+// //   }
+// // });
+
+// function allInput() {
+//   for (let i = 1; i <= 5; i++) {
+//     if (document.getElementById(`input${i}`).value === '') {
+//       return false;
+//     }
 //   }
+
+//   return true;
 // }
 
-// function phoneInput() {
-//   if (!document.getElementById('phone').value.match(/[a-zA-Z]/)) {
-//     return true;
-//   }
-// }
+// const submit1 = document.getElementById('submit1');
+// const invaliText = document.getElementById('invalid');
 
-// const input1 = document.getElementById('input1');
-// const text1 = document.getElementById('invalid1');
-
-// input1.addEventListener('blur', (e) => {
+// submit1.addEventListener('click', (e) => {
 //   e.preventDefault();
 
-//   if (nameInput(input1)) {
-//     input1.classList.remove('checkout__input--invalid');
-//     input1.classList.add('checkout__input--valid');
-//     text1.classList.remove('checkout__invalid--active');
+//   if (allInput()) {
+//     invaliText.classList.remove('checkout__invalid--active');
+//     // window.location.href = '../checkout2.html';
+//     window.open('../../checkout2.html');
+//     // location = '../checkout2.html';
+//     // window.location.replace = '../checkout2.html';
+//   } else {
+//     invaliText.classList.add('checkout__invalid--active');
 //   }
 // });
-
-// input1.addEventListener('blur', (e) => {
-//   e.preventDefault();
-
-//   if (!nameInput(input1)) {
-//     input1.classList.remove('checkout__input--valid');
-//     input1.classList.add('checkout__input--invalid');
-//     text1.classList.add('checkout__invalid--active');
-//   }
-// });
-
-// const input4 = document.getElementById('input4');
-// const text4 = document.getElementById('invalid4');
-
-// input4.addEventListener('blur', (e) => {
-//   e.preventDefault();
-
-//   if (phoneInput(input4)) {
-//     input4.classList.remove('checkout__input--invalid');
-//     input4.classList.add('checkout__input--valid');
-//     text4.classList.remove('checkout__invalid--active');
-//   }
-// });
-
-// input4.addEventListener('blur', (e) => {
-//   e.preventDefault();
-
-//   if (!nameInput(input4)) {
-//     input4.classList.remove('checkout__input--valid');
-//     input4.classList.add('checkout__input--invalid');
-//     text4.classList.add('checkout__invalid--active');
-//   }
-// });
-
-function allInput() {
-  for (let i = 1; i <= 5; i++) {
-    if (document.getElementById(`input${i}`).value === '') {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-const submit1 = document.getElementById('submit1');
-const invaliText = document.getElementById('invalid');
-
-submit1.addEventListener('click', (e) => {
-  e.preventDefault();
-
-  if (allInput()) {
-    invaliText.classList.remove('checkout__invalid--active');
-    // window.location.href = '../checkout2.html';
-    window.open('../../checkout2.html');
-    // location = '../checkout2.html';
-    // window.location.replace = '../checkout2.html';
-  } else {
-    invaliText.classList.add('checkout__invalid--active');
-  }
-});
