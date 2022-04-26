f463933ee64f879681e8c2957baef78cc82546bd
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-04-26 16:07:20 +0300

    fix

--- src/scripts/checkout.js
+++ src/scripts/checkout.js
@@ -78,7 +78,10 @@ submit1.addEventListener('click', (e) => {
 
   if (allInput()) {
     invaliText.classList.remove('checkout__invalid--active');
-    window.location.href = 'checkout2.html';
+    // window.location.href = '../checkout2.html';
+    window.open('../checkout2.html');
+    // location = '../checkout2.html';
+    // window.location.replace = '../checkout2.html';
   } else {
     invaliText.classList.add('checkout__invalid--active');
   }
