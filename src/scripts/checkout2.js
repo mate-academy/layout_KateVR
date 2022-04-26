28db5e40c0c4b179f2239ad052ca1b390c1435fc
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-04-26 18:39:53 +0300

    +js file

---
+++ src/scripts/checkout2.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const submit2 = document.getElementById('submit2');
+const invaliText2 = document.getElementById('invalid2');
+
+function allInput2() {
+  for (let i = 1; i <= 7; i++) {
+    if (document.getElementById(`card${i}`).value === '') {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+submit2.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (allInput2()) {
+    invaliText2.classList.remove('checkout__invalid--active');
+    // window.location.href = '../checkout2.html';
+    window.open('../checkout1.html');
+    // location = '../checkout2.html';
+    // window.location.replace = '../checkout2.html';
+  } else {
+    invaliText2.classList.add('checkout__invalid--active');
+  }
+});
