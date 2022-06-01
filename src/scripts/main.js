07cfd1f56eb5e14321e693ba0aab505081a72168
Yurii Sydorchuk <victance.3@gmail.com>
2022-06-01 16:56:27 +0300

    finished the basic layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const navMob = document.querySelector('.nav-mob');
+const navLink = document.querySelectorAll('.nav-mob__link');
+
+document.querySelector('#nav-mob-toggler')
+  .addEventListener('click', function(event) {
+    navMob.classList.add('page__nav-mob--active');
+    document.body.classList.add('page__body--with-menu');
+  }, true);
+
+document.querySelector('#nav-mob-toggler-2')
+  .addEventListener('click', function(event) {
+    navMob.classList.remove('page__nav-mob--active');
+    document.body.classList.remove('page__body--with-menu');
+  }, true);
+
+for (let i = 0; i < navLink.length; i++) {
+  navLink[i].addEventListener('click', function(event) {
+    navMob.classList.remove('page__nav-mob--active');
+    document.body.classList.remove('page__body--with-menu');
+  }, true);
+}
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleEvent);
+
+function handleEvent(event) {
+  event.preventDefault();
+  form.reset();
+}
