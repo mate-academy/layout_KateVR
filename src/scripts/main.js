38ba7c3eee378006abc1a15ccfb50ff6ae16a969
fledith <barateon023@gmail.com>
2022-06-16 14:40:36 +0300

    added tech` section and animation for about` section also create file for out block elements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,19 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#language') {
+    document.body.classList.add('page__body--with-language');
+  } else {
+    document.body.classList.remove('page__body--with-language');
+  }
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#help') {
+    document.body.classList.add('page__body--with-help');
+  } else {
+    document.body.classList.remove('page__body--with-help');
+  }
+});
