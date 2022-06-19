2fe626cbc7b9e9091ca1b53d2b7a079b5412c4de
fledith <barateon023@gmail.com>
2022-06-19 13:26:02 +0300

    added video block

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,8 @@ window.addEventListener('hashchange', () => {
 
   if (hash === '#menu' || hash === '#language' || hash === '#help') {
     document.body.classList.add('page__body--with-menu');
+  } else if (hash === '#video') {
+    document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
   }
