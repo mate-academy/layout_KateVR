78e66938e4c06998b7e7535f0cc9d34b2605a0f8
Denys Korotkoruchko <denenergii@gmail.com>
2022-06-21 16:58:45 +0300

    6/10

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
+/* eslint-disable no-unused-vars */
 'use strict';
+
+// eslint-disable-next-line no-undef
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  breakpoints: {
+    320: {
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    },
+    1224: {
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    },
+  },
+});
