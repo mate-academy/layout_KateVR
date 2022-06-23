9beac6ffc30fa96dfdebf0f359ab62ff1d63740a
Denys Korotkoruchko <denenergii@gmail.com>
2022-06-23 10:40:36 +0300

    is ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -25,3 +25,20 @@ const swiper = new Swiper('.swiper', {
     },
   },
 });
+
+const btn = document.getElementById('button');
+
+function magic() {
+  if (window.pageYOffset > 800) {
+    btn.style.opacity = '1';
+  } else {
+    btn.style.opacity = '0';
+  }
+}
+
+btn.onclick = function() {
+  window.scrollTo(0, 0);
+};
+
+// When scrolling, we run the function
+window.onscroll = magic;
