267577d6a3377ef2a210b8f4f76c2afecadd8640
silchyn <ivan.silchin@gmail.com>
2022-06-29 22:33:56 +0300

    added footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const form = document.querySelector('.touch__form');
+const more = document.querySelector('.more');
+const goTop = document.querySelector('.main__go-top');
+const footer = document.querySelector('.footer');
 
 form.addEventListener('submit', event => {
   event.preventDefault();
@@ -8,4 +11,13 @@ form.addEventListener('submit', event => {
   form.reset();
 
   window.location.href = '#';
+});
+
+window.addEventListener('scroll', () => {
+  if (more.getBoundingClientRect().top < 0
+    && footer.getBoundingClientRect().top >= window.innerHeight) {
+    goTop.style.display = 'block';
+  } else {
+    goTop.style.display = 'none';
+  }
 });
