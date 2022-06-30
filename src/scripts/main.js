da7af7c6216e2818594c93bd288578f17b780003
silchyn <ivan.silchin@gmail.com>
2022-06-30 20:43:00 +0300

    fixed links in menu & removed go top when menu is opened & fixed menu in landscape orientation & changed video source & added header image link & added slider & added language select & fixed autocompletion & added appearence animation to tech popups

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,13 @@ const form = document.querySelector('.touch__form');
 const more = document.querySelector('.more');
 const goTop = document.querySelector('.main__go-top');
 const footer = document.querySelector('.footer');
+const menuList = document.querySelector('.header__menu-list');
+const menuToggler = document.querySelector('.header__burger-toggler');
+const aboutPrev = document.querySelector('.about__prev');
+const aboutNext = document.querySelector('.about__next');
+const aboutSlideshow = document.querySelector('.about__slideshow');
+const aboutCounter = document.querySelector('.about__counter');
+let counter = 1;
 
 form.addEventListener('submit', event => {
   event.preventDefault();
@@ -20,4 +27,32 @@ window.addEventListener('scroll', () => {
   } else {
     goTop.style.display = 'none';
   }
+});
+
+menuList.addEventListener('click', () => {
+  menuToggler.checked = false;
+});
+
+aboutPrev.addEventListener('click', () => {
+  --counter;
+
+  if (counter < 1) {
+    counter += 5;
+  }
+
+  aboutSlideshow.style.left = `${(counter - 1)
+  * -1 * aboutSlideshow.offsetWidth / 5}px`;
+  aboutCounter.textContent = `${counter}/5`;
+});
+
+aboutNext.addEventListener('click', () => {
+  ++counter;
+
+  if (counter > 5) {
+    counter -= 5;
+  }
+
+  aboutSlideshow.style.left = `${(counter - 1)
+    * -1 * aboutSlideshow.offsetWidth / 5}px`;
+  aboutCounter.textContent = `${counter}/5`;
 });
