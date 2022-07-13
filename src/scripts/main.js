bb13aea97430c385572c62867964b7718b04709a
Yevhen Pavlovskyi <pavlovskiy07@gmail.com>
2022-07-13 11:38:18 +0300

    started

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const button = document.getElementById('Mbtn');
+
+button.addEventListener('click', () => {
+  document.getElementById('more').classList.toggle('more--active');
+});
+
+// const dot =
+// document.getElementsByClassName('about__container-video-nav-dot');
+
+// dot.addEventListener('click', () => {
+//   dot.classList.remove('about__container-video-nav-dot--active');
+//   dot.this.document.classList.add('about__container-video-nav-dot--active');
+// });
