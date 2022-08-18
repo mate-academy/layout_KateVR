3c460bc15d54f1eff547d47522a1f16e6b5e5cd3
Vitalii Lola <lola1v41@gmail.com>
2022-08-18 21:00:16 +0300

    kateVR 002

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,7 +12,7 @@ form.addEventListener('submit', function(event) {
 });
 
 window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
+  if (window.location.hash === '#menu-drop') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
