5239aa0177addb2d9f75e1f76b8ce67b6add5082
naveed-shoukat <shoukat.naveed@gmail.com>
2022-09-14 01:01:28 +0300

    faqs section addedw with required styles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,15 @@
 
 const heroMenu = document.querySelector('.hero__menu');
 const heroNav = document.querySelector('.nav');
+
 const languageLink = document.querySelector('.nav__link--lang');
-const lnaguageBackBtn = document.querySelector('.language-menu__back-btn');
+const langBackBtn = document.querySelector('.language-menu__back-btn');
 const languageMenu = document.querySelector('.language-menu');
+
+const faqsLink = document.querySelector('.nav__link--faq');
+const bottomFaqsLink = document.querySelector('.bottom-nav__link--faq');
+const faqsBackBtn = document.querySelector('.faq__back-btn');
+const faqs = document.querySelector('.faq');
 
 heroMenu.addEventListener('click', () => {
   heroMenu.classList.toggle('hero__menu--cros-icon');
@@ -17,6 +23,18 @@ languageLink.addEventListener('click', () => {
   languageMenu.classList.add('language-menu--is-open');
 });
 
-lnaguageBackBtn.addEventListener('click', () => {
+langBackBtn.addEventListener('click', () => {
   languageMenu.classList.remove('language-menu--is-open');
+});
+
+faqsLink.addEventListener('click', () => {
+  faqs.classList.add('faq--is-open');
+});
+
+bottomFaqsLink.addEventListener('click', () => {
+  faqs.classList.add('faq--is-open');
+});
+
+faqsBackBtn.addEventListener('click', () => {
+  faqs.classList.remove('faq--is-open');
 });
