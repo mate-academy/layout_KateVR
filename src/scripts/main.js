32f2130dcf12890830f9229fd7da534bc69a328f
Nazar Brunarskyi <3nnazar836@gmail.com>
2022-11-11 17:45:28 +0200

    finished an adaptation of the menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  const hashArr = ['#menu-mobile', '#Languages', '#Help'];
+
+  if (hashArr.includes(window.location.hash)) {
+    window.scrollTo(0, 0);
+    document.body.classList.add('body--scroll-off');
+  } else {
+    document.body.classList.remove('body--scroll-off');
+  }
+});
