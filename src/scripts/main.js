709d7b5e51eaddbf5f8460820f83a4676c9d1104
Denis Bokov <denisbokov1703@gmail.com>
2022-11-14 19:00:58 +0100

    KateVR (1)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,55 @@
 'use strict';
+
+// const Swiper = require('swiper');
+
+// const swiper = new Swiper('.swiper', {
+//   loop: true,
+
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+
+//   navigation: {
+//     // nextEl: '.swiper-button-next',
+//     // prevEl: '.swiper-button-prev',
+//   },
+
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+
+//   parserOptions: {
+//     sourceType: 'module',
+//   },
+// });
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+const forms = document.querySelectorAll('.form');
+
+const formField = document.querySelector('.form__field');
+
+formField.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  forms.forEach((form) => {
+    form.value = '';
+  });
+});
