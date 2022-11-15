4acb7e1587fab0c3227e73298bdfa5935303de55
Denis Bokov <denisbokov1703@gmail.com>
2022-11-15 22:08:18 +0100

    KateVR (2)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -53,3 +53,11 @@ formField.addEventListener('submit', (event) => {
     form.value = '';
   });
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__content--with-menu');
+  } else {
+    document.body.classList.remove('page__content--with-menu');
+  }
+});
