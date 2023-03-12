d2b277aeeb1d76783ee846154186283978ef3ddf
DanielSorokowski <sorokowskidaniel0@gmail.com>
2023-03-12 16:53:35 +0100

    language block

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 'use strict';
+// MOBILE NAV
 
 const mobileMenuOpen = document.querySelector('#mobile-menu-open');
 const mobileMenuClose = document.querySelector('#mobile-menu-close');
@@ -10,4 +11,46 @@ mobileMenuOpen.addEventListener('click', () => {
 
 mobileMenuClose.addEventListener('click', () => {
   nav.classList.remove('nav--show');
+});
+
+// MOBILE LANG
+const mobileLangTrigger = document.querySelector('#mobile-lang-trigger');
+const languageMobileClose = document.querySelector('#language-mobile-close');
+const language = document.querySelector('.language');
+
+mobileLangTrigger.addEventListener('click', () => {
+  language.classList.add('language--show');
+});
+
+languageMobileClose.addEventListener('click', () => {
+  language.classList.toggle('language--show');
+});
+
+// DESKTOP LANG
+const languageBtn = document.querySelectorAll('.language__btn');
+const languageBack = document.querySelector('.language__back');
+const languageBtnActive = document.querySelector('.language__btn--active');
+const langInner = () => {
+  if (window.innerWidth >= 1280) {
+    languageBack.innerHTML = languageBtnActive.getAttribute('data-short');
+
+    languageBtn.forEach(btn => {
+      btn.innerText = btn.getAttribute('data-short');
+    });
+  } else {
+    languageBack.innerHTML = '';
+
+    languageBtn.forEach(btn => {
+      btn.innerText = btn.getAttribute('data-name');
+    });
+  }
+};
+
+window.addEventListener('load', langInner);
+window.addEventListener('resize', langInner);
+
+languageBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    languageBack.innerHTML = btn.innerText;
+  });
 });
