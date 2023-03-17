d33f939360f5bf39dfcb1aa16ac41dda57ba68b5
Vitalii Parkhomenko <poroh1991ukr@gmail.com>
2023-03-18 01:43:37 +0200

    done header-submenu-faq

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,39 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const burgerButtonHeader = document.querySelector('.header__burger');
+  const crossButtonMenu = document.querySelectorAll('.menu__cross');
+  const body = document.querySelector('.body');
+  const menuHeader = document.querySelector('.header__menu');
+  const menuItemHasSub = document.querySelectorAll('.menu__link--has-sub');
+  const menuItemSubArrow = document.querySelectorAll('.arrow-back');
+
+  burgerButtonHeader.addEventListener('click', () => {
+    body.classList.toggle('lock');
+    menuHeader.classList.toggle('active');
+  });
+
+  crossButtonMenu.forEach(cross => {
+    cross.addEventListener('click', () => {
+      if (window.location.hash) {
+        window.location.hash = '';
+      }
+
+      if (cross.closest('.active')) {
+        cross.closest('.active').classList.remove('active');
+      }
+    });
+  });
+
+  menuItemHasSub.forEach(item => {
+    item.addEventListener('click', () => {
+      item.nextElementSibling.classList.toggle('active');
+    });
+  });
+
+  menuItemSubArrow.forEach(arrow => {
+    arrow.addEventListener('click', () => {
+      arrow.closest('.active').classList.remove('active');
+    });
+  });
+});
