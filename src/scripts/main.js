97dafcc26e23ca20b54506d48083eb8b986679eb
Yelyzaveta <molochiza@gmail.com>
2023-03-21 20:57:34 +0200

    1st commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  form.reset();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#language') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const swiper = new Swiper('.about-us__swiper', {
+  direction: 'horizontal',
+  loop: true,
+  effect: 'slide',
+  grabCursor: true,
+  centeredSlides: true,
+
+  pagination: {
+    el: '.about-us__pagination',
+    clickable: true,
+  }
+});
