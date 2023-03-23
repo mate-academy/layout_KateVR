80999670e992fb66e3dc88866f37cc04797221b8
Vo7kov <drainys1234@gmail.com>
2023-03-23 11:39:47 +0100

    fix form submit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
+import './blocks/formValidation';
 import './blocks/slider';
 import './blocks/video';
 import './blocks/techButtons';
 import './blocks/formInputshover';
-import './blocks/formValidation';
