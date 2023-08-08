960ba819662160bbc051dfbaec397b97cc689b81
Anastasia Granovskaya <iamgranovskaya@gmail.com>
2023-08-08 12:17:36 +0300

    menu with language,faq,help

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+window.addEventListener('load', () => {
+  for (const element of document.getElementsByClassName('faq__question')) {
+    element.addEventListener('click', (e) => {
+      if (e.target.nextElementSibling.classList.contains('faq__open--is')) {
+        e.target.nextElementSibling.classList.remove('faq__open--is');
+      } else {
+        e.target.nextElementSibling.classList.add('faq__open--is');
+      }
+    });
+  }
+});
