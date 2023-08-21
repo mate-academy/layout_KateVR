b96abf03f36bb060316ab9ed559370dc70dc8214
Olena Shapkun <shapkunkh@gmail.com>
2023-08-21 15:43:28 +0300

    main, footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const accordionButtons = document.querySelectorAll('.faq__accordion');
+
+accordionButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const accordionInfo = button.nextElementSibling;
+
+    if (accordionInfo.style.display === 'none'
+      || accordionInfo.style.display === '') {
+      const allAccordionInfos = document.querySelectorAll('.faq__accordion-info'); // eslint-disable-line
+
+      allAccordionInfos.forEach(info => {
+        info.style.display = 'none';
+      });
+
+      accordionInfo.style.display = 'block';
+    } else {
+      accordionInfo.style.display = 'none';
+    }
+  });
+});
