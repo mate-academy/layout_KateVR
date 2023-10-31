667b49ca4846179f122b03a64970bfdbba37f01a
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2023-10-31 14:28:12 +0100

    Change select for custom select

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,74 @@
 'use strict';
+
+const customSelects = document.getElementsByClassName('custom-select');
+
+for (let i = 0; i < customSelects.length; i++) {
+  const selectElement = customSelects[i].getElementsByTagName('select')[0];
+  const options = selectElement.options;
+
+  const selectedDiv = document.createElement('DIV');
+
+  selectedDiv.setAttribute('class', 'select-selected');
+  selectedDiv.innerHTML = options[selectElement.selectedIndex].innerHTML;
+  customSelects[i].appendChild(selectedDiv);
+
+  const optionListDiv = document.createElement('DIV');
+
+  optionListDiv.setAttribute('class', 'select-items select-hide');
+
+  for (let j = 1; j < options.length; j++) {
+    const optionDiv = document.createElement('DIV');
+
+    optionDiv.innerHTML = options[j].innerHTML;
+
+    optionDiv.addEventListener('click', function() {
+      const clickedOptionText = this.innerHTML;
+
+      selectElement.value = clickedOptionText;
+      selectedDiv.innerHTML = clickedOptionText;
+
+      const selectedItems
+        = optionListDiv.getElementsByClassName('same-as-selected');
+
+      for (let k = 0; k < selectedItems.length; k++) {
+        selectedItems[k].classList.remove('same-as-selected');
+      }
+      this.classList.add('same-as-selected');
+      selectedDiv.click();
+    });
+
+    optionListDiv.appendChild(optionDiv);
+  }
+
+  customSelects[i].appendChild(optionListDiv);
+
+  selectedDiv.addEventListener('click', function(e) {
+    e.stopPropagation();
+    closeAllSelect(this);
+    optionListDiv.classList.toggle('select-hide');
+    this.classList.toggle('select-arrow-active');
+  });
+}
+
+function closeAllSelect(clickedSelect) {
+  const selectItems = document.getElementsByClassName('select-items');
+  const selectedItems = document.getElementsByClassName('select-selected');
+
+  for (let i = 0; i < selectedItems.length; i++) {
+    if (clickedSelect === selectedItems[i]) {
+      continue;
+    }
+    selectedItems[i].classList.remove('select-arrow-active');
+  }
+
+  for (let i = 0; i < selectItems.length; i++) {
+    if (selectItems[i].style.display !== 'none'
+      && selectItems[i].previousSibling !== clickedSelect) {
+      selectItems[i].classList.add('select-hide');
+    }
+  }
+}
+
+document.addEventListener('click', function() {
+  closeAllSelect();
+});
