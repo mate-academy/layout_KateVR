9d38197b0027aa93b7d92f8dcbfd326807349f70
Inna Sheremet <innsheremet88@gmail.com>
2023-12-21 18:24:39 +0200

    sections

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -176,36 +176,162 @@ document.addEventListener('DOMContentLoaded', function() {
 
 /* вимкнути скрол при відкритих додаткових вікнах */
 
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu'
-    || window.location.hash === '#buy'
-    || window.location.hash === '#language'
-    || window.location.hash === '#faq'
-    || window.location.hash === '#help'
-    || window.location.hash === '#buy') {
-    document.body.classList.add('page__body-menu');
-  } else {
-    document.body.classList.remove('page__body-menu');
-  }
+document.addEventListener('DOMContentLoaded', function() {
+  const openButton = document.getElementById('open-faq');
+  const faqWindow = document.getElementById('faq');
+  const body = document.body;
+  const closeButton = document.getElementById('cross');
+
+  openButton.addEventListener('click', function() {
+    faqWindow.style.display = 'block';
+    body.classList.add('faq-open');
+  });
+
+  // Закриття вікна FAQ при кліку на елемент з id="cross"
+  closeButton.addEventListener('click', function() {
+    faqWindow.style.display = 'none';
+    body.classList.remove('faq-open');
+  });
+
+  // Опційно: закривайте вікно FAQ при кліку на затемнений фон
+  faqWindow.addEventListener('click', function(event) {
+    if (event.target === faqWindow) {
+      faqWindow.style.display = 'none';
+      body.classList.remove('faq-open');
+    }
+  });
 });
 
-/* ввімкнути скрол при відкритому вікні faq, help */
+/* рахуємо вартість одиниць товарів */
 
 document.addEventListener('DOMContentLoaded', function() {
-  const faqElement = document.getElementById('faq');
+  const qualityButton = document.getElementById('qualityButton');
+  const selectValue = document.getElementById('selectValue');
+  const priceDisplay = document.getElementById('priceDisplay');
+  const initialPrice = 1200;
 
-  function setVerticalScroll() {
-    const windowHeight = window.innerHeight;
-    const faqHeight = faqElement.scrollHeight;
+  selectValue.value = qualityButton.innerText;
+  priceDisplay.innerText = initialPrice * selectValue.value + '$';
 
-    if (faqHeight > windowHeight) {
-      faqElement.style.overflowY = 'auto';
+  document
+    .querySelectorAll('.select-buy__quality-option')
+    .forEach(function(option) {
+      option.addEventListener('click', function() {
+        const selectedValue = option.getAttribute('data-value');
+
+        selectValue.value = selectedValue;
+        priceDisplay.innerText = initialPrice * selectedValue + '$';
+      });
+    });
+});
+
+/* валідатор телефонного номеру */
+
+/* очищення форми при кліку на кнопку form-buy__purchase */
+
+const buyButton = document.querySelector('.form-buy__purchase');
+
+buyButton.addEventListener('click', function() {
+  clearForm();
+});
+
+function clearForm() {
+  const formPlace = document.getElementById('form-place');
+  const formPay = document.getElementById('form-pay');
+
+  formPlace.querySelectorAll('input').forEach(input => {
+    input.value = '';
+  });
+
+  formPay.querySelectorAll('input').forEach(input => {
+    input.value = '';
+  });
+
+  const qualityButton = document.getElementById('qualityButton');
+  const priceDisplay = document.getElementById('priceDisplay');
+  const initialPrice = 1200;
+
+  qualityButton.textContent = '1';
+  priceDisplay.innerText = initialPrice + '$';
+}
+
+/* змінюємо активну кнопку у блоку #buy, приховуємо і відображаємо блоки */
+
+document.addEventListener('DOMContentLoaded', function() {
+  const buttons = document.querySelectorAll('.button-buy__link');
+  const blocks = {
+    'Place Order': ['place-buy', 'form-place'],
+    'Pay': ['form-pay', 'place-buy'],
+    'Complete': ['complete-buy'],
+  };
+
+  function activateFirstButton() {
+    buttons[0].classList.add('button-buy__link-active');
+
+    blocks[buttons[0].textContent].forEach(id => {
+      const block = document.getElementById(id);
+
+      if (block) {
+        block.classList.remove('hidden');
+      }
+    });
+  }
+
+  setTimeout(activateFirstButton, 0);
+
+  function handleWindowResize() {
+    const windowWidth = window.innerWidth;
+    const activeButton = document.querySelector('.button-buy__link-active');
+
+    if (windowWidth >= 768
+      && windowWidth <= 1280
+      && activeButton
+      && activeButton.textContent === 'Pay') {
+      const placeBuyBlock = document.getElementById('place-buy');
+
+      if (placeBuyBlock) {
+        placeBuyBlock.style.width = '460px';
+        placeBuyBlock.style.margin = '0 auto';
+      }
     } else {
-      faqElement.style.overflowY = 'hidden';
+      // Скидання стилів блоку, якщо умова не виконується
+      const placeBuyBlock = document.getElementById('place-buy');
+
+      if (placeBuyBlock) {
+        placeBuyBlock.style.width = ''; // Скидання ширини
+        placeBuyBlock.style.margin = ''; // Скидання відступів
+      }
     }
   }
 
-  setVerticalScroll();
+  // Додавання обробника події resize до вікна
+  window.addEventListener('resize', handleWindowResize);
 
-  window.addEventListener('resize', setVerticalScroll);
+  buttons.forEach(button => {
+    button.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      Object.values(blocks).flat().forEach(id => {
+        const block = document.getElementById(id);
+
+        if (block) {
+          block.classList.add('hidden');
+        }
+      });
+
+      blocks[this.textContent].forEach(id => {
+        const block = document.getElementById(id);
+
+        if (block) {
+          block.classList.remove('hidden');
+        }
+      });
+
+      buttons.forEach(btn => btn.classList.remove('button-buy__link-active'));
+      this.classList.add('button-buy__link-active');
+
+      // При зміні кнопки також перевіряємо розмір вікна
+      handleWindowResize();
+    });
+  });
 });
