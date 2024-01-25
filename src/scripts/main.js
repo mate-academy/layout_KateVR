b5e58731fd7faa4496c7efa01271c19a77dae0c4
Ihor Prodan <prodanigor0@gmail.com>
2024-01-25 22:01:30 +0100

    lending page KatVR

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,73 @@
 'use strict';
+
+document.querySelectorAll('.buy-button');
+
+document.body.addEventListener('click', function(event) {
+  if (event.target.classList.contains('buy-button')) {
+    redirectToOtherPage();
+  }
+});
+
+function redirectToOtherPage() {
+  window.location.href = '#place-order';
+}
+
+document.getElementById('header__button-conteiner');
+
+document.body.addEventListener('click', function(event) {
+  if (event.target.classList.contains('buyVideo')) {
+    redirectToOtherPageVideo();
+  }
+});
+
+function redirectToOtherPageVideo() {
+  window.location.href = '#video';
+}
+
+const buyOrder = document.getElementById('place-order__button-id');
+
+buyOrder.addEventListener('click', function() {
+  redirectToOtherPageOrder();
+});
+
+function redirectToOtherPageOrder() {
+  window.location.href = '#pay';
+}
+
+const buyPay = document.getElementById('pay-id');
+
+buyPay.addEventListener('click', function() {
+  redirectToOtherPagePay();
+});
+
+function redirectToOtherPagePay() {
+  window.location.href = '#complete';
+}
+
+const buyComplete = document.getElementById('complete-id');
+
+buyComplete.addEventListener('click', function() {
+  redirectToOtherPageComplete();
+});
+
+function redirectToOtherPageComplete() {
+  window.location.href = '#';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Отримуємо посилання на елементи
+  const showButton = document.getElementById('icon-id');
+  const hiddenElement = document.getElementById('tech_window');
+
+  // Додаємо обробник події для кліку на кнопці
+  showButton.addEventListener('click', function() {
+    // Перевіряємо, чи елемент прихований
+    if (hiddenElement.style.display === 'none') {
+      // Якщо так, робимо його видимим
+      hiddenElement.style.display = 'block';
+    } else {
+      // Якщо ні, приховуємо його
+      hiddenElement.style.display = 'none';
+    }
+  });
+});
