632e7bc390003cad55b116ff434f9241b427ad11
Ihor Prodan <prodanigor0@gmail.com>
2024-01-28 13:32:32 +0100

    add task solution KateVrr

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,79 +1,28 @@
 'use strict';
 
-document.querySelectorAll('.buy-button');
-
-document.body.addEventListener('click', function(event) {
-  if (event.target.classList.contains('buy-button')) {
-    redirectToOtherPage();
-  }
-});
-
-function redirectToOtherPage() {
-  window.location.href = '#place-order';
-}
-
-document.getElementById('header__button-conteiner');
-
-document.body.addEventListener('click', function(event) {
-  if (event.target.classList.contains('buyVideo')) {
-    redirectToOtherPageVideo();
-  }
-});
-
-function redirectToOtherPageVideo() {
-  window.location.href = '#video';
-}
-
-document.getElementById('complete-id');
-
-document.body.addEventListener('click', function(event) {
-  if (event.target.classList.contains('complete-id')) {
-    redirectToOtherPageHome();
-  }
-});
-
-function redirectToOtherPageHome() {
-  window.location.href = '#';
-}
-
-document.querySelectorAll('.complete__button');
-
-document.body.addEventListener('click', function(event) {
-  if (event.target.classList.contains('complete__button')) {
-    redirectToOtherHome();
-  }
-});
-
-function redirectToOtherHome() {
-  window.location.href = '#';
-}
-
-document.body.addEventListener('click', function(event) {
-  if (event.target.classList.contains('place-order__button')) {
-    const form = document.querySelector('.place-order__forms');
-
-    if (form.checkValidity()) {
-      redirectToOtherPay();
-    } else {
+function redirectToOtherPage(targetClass, destination) {
+  document.body.addEventListener('click', function(event) {
+    if (event.target.classList.contains(targetClass)) {
+      window.location.href = destination;
     }
-  }
-});
-
-function redirectToOtherPay() {
-  window.location.href = '#pay';
+  });
 }
 
-document.body.addEventListener('click', function(event) {
-  if (event.target.classList.contains('pay__button')) {
-    const form = document.querySelector('.pay__forms');
+function redirectToOtherForm(formClass, destination) {
+  document.body.addEventListener('click', function(event) {
+    if (event.target.classList.contains('place-order__button')) {
+      const form = document.querySelector(formClass);
 
-    if (form.checkValidity()) {
-      redirectToOtherComplete();
-    } else {
+      if (form.checkValidity()) {
+        window.location.href = destination;
+      }
     }
-  }
-});
-
-function redirectToOtherComplete() {
-  window.location.href = '#complete';
+  });
 }
+
+redirectToOtherPage('buy-button', '#place-order');
+redirectToOtherPage('buyVideo', '#video');
+redirectToOtherPage('complete-id', '#');
+redirectToOtherPage('complete__button', '#');
+redirectToOtherForm('.pay__forms', '#pay');
+redirectToOtherForm('.complete__forms', '#complete');
