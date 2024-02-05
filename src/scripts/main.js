9f7ae92c10a850754a990b79dd51778d5cd46625
Mykhailiuk Viktor <117285070+Viktor535x@users.noreply.github.com>
2024-02-05 15:48:30 +0200

    test5

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,7 +10,9 @@
 // form.addEventListener('submit', submit);
 
 window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menuR') {
+  if (window.location.hash === '#menuR' || window.location.hash
+  === '#menuX2' || window.location.hash
+  === '#LANGUAGE' | window.location.hash === '#menuH') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
