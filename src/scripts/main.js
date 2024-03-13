f1d0f2891ff0a900e2d1a225d59221dcf0b6953d
Fedos17 <dfkd81@gmail.com>
2024-03-13 12:05:39 +0200

    the language selection block has been updated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,37 +1,22 @@
 'use strict';
 
-// dropdown menu
+// language menu
+const htmlLang = document.querySelector('.html');
+const dropdownButton = document.querySelector('.dropdown__button');
+const dropdownList = document.querySelectorAll('.dropdown__list');
+const menuList = document.querySelectorAll('.menu__lang-link');
+
+let langHref = 'en';
+let langName = 'En';
+
 const dropdown = () => {
-  const dropdownButton = document.querySelector('.dropdown__button');
   const dropdownMenu = document.querySelector('.dropdown__menu');
-
-  const selectLang = () => {
-    const htmlLang = document.querySelector('.html');
-    const elementLang = document.querySelectorAll('.dropdown__list');
-
-    for (let i = 0; i < elementLang.length; i++) {
-      const langHref = elementLang[i].children[0].href.match(/#(.*)/)[1];
-      const langName = elementLang[i].children[0].textContent;
-
-      if (langHref !== htmlLang.lang) {
-        elementLang[i].classList.remove('dropdown__list--active');
-      }
-
-      elementLang[i].addEventListener('click', (event) => {
-        htmlLang.lang = langHref;
-        dropdownButton.textContent = langName;
-        elementLang[i].classList.add('dropdown__list--active');
-      });
-    }
-  };
 
   dropdownButton.addEventListener('click', (event) => {
     event.preventDefault();
 
     dropdownMenu.classList.toggle('dropdown__menu--active');
     dropdownButton.classList.toggle('dropdown__button--active');
-
-    selectLang();
   });
 
   document.addEventListener('click', (event) => {
@@ -41,6 +26,56 @@ const dropdown = () => {
     }
   });
 };
+
+const updateDropdown = () => {
+  for (let i = 0; i < dropdownList.length; i++) {
+    if (langHref === dropdownList[i].children[0].href.match(/#(.*)/)[1]) {
+      dropdownButton.textContent = langName;
+      dropdownList[i].classList.add('dropdown__list--active');
+    } else {
+      dropdownList[i].classList.remove('dropdown__list--active');
+    }
+  }
+};
+
+const updateMenu = () => {
+  for (let i = 0; i < menuList.length; i++) {
+    if (langHref === menuList[i].href.match(/#(.*)/)[1]) {
+      menuList[i].classList.add('menu__lang-link--active');
+    } else {
+      menuList[i].classList.remove('menu__lang-link--active');
+    }
+  }
+};
+
+const selectLangInDropdown = () => {
+  for (let i = 0; i < dropdownList.length; i++) {
+    dropdownList[i].addEventListener('click', (event) => {
+      langHref = dropdownList[i].children[0].href.match(/#(.*)/)[1];
+      langName = dropdownList[i].children[0].textContent;
+
+      htmlLang.lang = langHref;
+
+      updateDropdown();
+      updateMenu();
+    });
+  }
+};
+
+const selectLangInMenu = () => {
+  for (let i = 0; i < menuList.length; i++) {
+    menuList[i].addEventListener('click', (event) => {
+      langHref = menuList[i].href.match(/#(.*)/)[1];
+      langName = menuList[i].name;
+
+      htmlLang.lang = langHref;
+
+      updateDropdown();
+      updateMenu();
+    });
+  }
+};
+// end language menu
 
 // header navigation
 const navigation = () => {
@@ -59,4 +94,6 @@ const navigation = () => {
 };
 
 dropdown();
+selectLangInDropdown();
+selectLangInMenu();
 navigation();
