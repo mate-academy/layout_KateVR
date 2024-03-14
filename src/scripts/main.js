7626e274cc288db973734d029ad25028fe0a889f
Oleksandr Kovalchuk <kov280396@gmail.com>
2024-03-14 16:37:57 +0200

    implement top-bar section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const dropdown = document.querySelector('.dropdown');
+const trigger = document.querySelector('.dropdown__trigger');
+const inProcess = document.querySelectorAll('.in-process');
+
+trigger.addEventListener('click', () => {
+  dropdown.classList.toggle('dropdown--active');
+});
+
+for (let i = 0; i < inProcess.length; i++) {
+  inProcess[i].addEventListener('click', () => {
+    alert('The feature is still under development');
+  });
+}
