df3c99447c99628bea2e9a18766379e3777cd87d
Vladyslav <vladbuldenko@gmail.com>
2024-04-11 21:53:08 +0300

    VB solution partly solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const dropdown = document.querySelectorAll('.dropdown');
+const trigger = document.querySelectorAll('.dropdown__trigger');
+const dev = document.querySelectorAll('.indev');
+
+for (let i = 0; i < trigger.length; i++) {
+  trigger[i].addEventListener('click', () => {
+    dropdown[i].classList.toggle('dropdown--active');
+  });
+}
+
+for (let i = 0; i < dev.length; i++) {
+  dev[i].addEventListener('click', () => {
+    alert('The feature is still under development');
+  });
+}
