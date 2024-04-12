9f034f02dbd46fa1326d31f70ca7bd2d08249b88
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2024-04-12 14:01:52 +0300

    header some revisions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,3 +15,19 @@ for (let i = 0; i < dev.length; i++) {
     alert('The feature is still under development');
   });
 }
+
+const tech1 = document.querySelector('.tech__dot--1');
+const tech2 = document.querySelector('.tech__dot--2');
+const tech3 = document.querySelector('.tech__dot--3');
+
+const techInfo1 = document.querySelector('.tech__info--1');
+const techInfo2 = document.querySelector('.tech__info--2');
+const techInfo3 = document.querySelector('.tech__info--3');
+
+function toggleClassName(el) {
+  el.classList.toggle('tech__info--visible');
+}
+
+tech1.addEventListener('click', () => toggleClassName(techInfo1));
+tech2.addEventListener('click', () => toggleClassName(techInfo2));
+tech3.addEventListener('click', () => toggleClassName(techInfo3));
