4eed7767bb288ce509f26ada74462ad7991dc11f
Ihor Prodan <prodanigor0@gmail.com>
2024-05-26 11:29:53 +0200

    landing-updater

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -29,3 +29,99 @@ document.body.addEventListener('click', function(event) {
     }
   }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector('.contact__forms');
+  const inputs = form.querySelectorAll('.contact__input, .contact__textarea');
+
+  inputs.forEach(input => {
+    input.addEventListener('input', function() {
+      if (this.type === 'number') {
+        if (this.value === '' || !isNaN(this.value)) {
+          this.classList.remove('invalid');
+        } else {
+          this.classList.add('invalid');
+        }
+      } else if (this.value === '' || this.checkValidity()) {
+        this.classList.remove('invalid');
+      } else {
+        this.classList.add('invalid');
+      }
+    });
+  });
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let isValid = true;
+
+    inputs.forEach(input => {
+      if (input.type === 'number') {
+        if (input.value !== '' && isNaN(input.value)) {
+          isValid = false;
+          input.classList.add('invalid');
+        } else {
+          input.classList.remove('invalid');
+        }
+      } else if (input.value !== '' && !input.checkValidity()) {
+        isValid = false;
+        input.classList.add('invalid');
+      } else {
+        input.classList.remove('invalid');
+      }
+    });
+
+    if (isValid) {
+      form.reset();
+      inputs.forEach(input => input.classList.remove('invalid'));
+    }
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector('.pay__forms');
+  const inputs = form.querySelectorAll('.pay__card-number, .pay__card-number--2, .pay__card-number--3, .pay__card-number--4, .pay__card-holder-name, .pay__date, .pay__card-cvv');
+
+  inputs.forEach(input => {
+    input.addEventListener('input', function() {
+      if (this.type === 'number') {
+        if (this.value === '' || !isNaN(this.value)) {
+          this.classList.remove('invalid');
+        } else {
+          this.classList.add('invalid');
+        }
+      } else if (this.value === '' || this.checkValidity()) {
+        this.classList.remove('invalid');
+      } else {
+        this.classList.add('invalid');
+      }
+    });
+  });
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let isValid = true;
+
+    inputs.forEach(input => {
+      if (input.type === 'number') {
+        if (input.value !== '' && isNaN(input.value)) {
+          isValid = false;
+          input.classList.add('invalid');
+        } else {
+          input.classList.remove('invalid');
+        }
+      } else if (input.value !== '' && !input.checkValidity()) {
+        isValid = false;
+        input.classList.add('invalid');
+      } else {
+        input.classList.remove('invalid');
+      }
+    });
+
+    if (isValid) {
+      form.reset();
+      inputs.forEach(input => input.classList.remove('invalid'));
+    }
+  });
+});
+
+
