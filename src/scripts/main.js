3b0ddc9d240986523035c66f2f790897f842d7d9
Pavel Maistrenko <maystrenko.p@gmail.com>
2024-08-16 16:17:20 +0200

    added video

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const playButton = [...document.querySelectorAll(".play-button")];
+const closeVideoScreen = document.querySelector(".close-video__button");
+const videoScreen = document.querySelector(".video-container");
+
+playButton[0].addEventListener("click", () => {
+  videoScreen.classList.remove("hidden");
+});
+
+playButton[1].addEventListener("click", () => {
+  videoScreen.classList.remove("hidden");
+});
+
+closeVideoScreen.addEventListener("click", () => {
+  videoScreen.classList.add("hidden");
+});
