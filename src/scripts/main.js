9f5c8a71586dd8cdbb7bf9edf78f6496ddc24d32
Pavel Maistrenko <maystrenko.p@gmail.com>
2024-08-19 13:46:51 +0200

    implement video block

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,25 @@
 "use strict";
 
 const body = document.querySelector(".page__body");
-const menuOpenButton = document.querySelector(".menu-open-button");
 const playButton = [...document.querySelectorAll(".play-button")];
 const closeVideoScreen = document.querySelector(".close-video__button");
 const videoScreen = document.querySelector(".video-container");
+const darkBackground = document.querySelector(".dark-background");
 
 playButton[0].addEventListener("click", () => {
   videoScreen.classList.remove("hidden");
   body.classList.add("overflow--hidden");
-  menuOpenButton.classList.add("hidden");
+  darkBackground.classList.remove("hidden");
 });
 
 playButton[1].addEventListener("click", () => {
   videoScreen.classList.remove("hidden");
   body.classList.add("overflow--hidden");
-  menuOpenButton.classList.add("hidden");
+  darkBackground.classList.remove("hidden");
 });
 
 closeVideoScreen.addEventListener("click", () => {
   videoScreen.classList.add("hidden");
   body.classList.remove("overflow--hidden");
-  menuOpenButton.classList.remove("hidden");
+  darkBackground.classList.add("hidden");
 });
