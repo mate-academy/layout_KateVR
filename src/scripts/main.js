1e5248b9b4e433af29138a06e281e0ec3f1be471
Pavel Maistrenko <maystrenko.p@gmail.com>
2024-08-20 19:28:12 +0200

    implement slider func

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+/* play video funccional */
+
 const body = document.querySelector(".page__body");
 const playButton = [...document.querySelectorAll(".play-button")];
 const closeVideoScreen = document.querySelector(".close-video__button");
@@ -23,3 +25,29 @@ closeVideoScreen.addEventListener("click", () => {
   body.classList.remove("overflow--hidden");
   darkBackground.classList.add("hidden");
 });
+
+/* slider functional */
+
+const sliderScreen = document.querySelector(".about__poster");
+const sliderDots = document.querySelectorAll(".nav__dot");
+let slideNum = 1;
+
+setInterval(() => {
+  if (!videoScreen.classList.contains("hidden")) {
+    return 0;
+  } else {
+    if (slideNum < 4) {
+      sliderScreen.classList.remove(`img-${slideNum}`);
+      sliderDots[slideNum - 1].classList.remove("nav__dot--active");
+      slideNum++;
+      sliderDots[slideNum - 1].classList.add("nav__dot--active");
+      sliderScreen.classList.add(`img-${slideNum}`);
+    } else {
+      slideNum = 1;
+      sliderDots[3].classList.remove("nav__dot--active");
+      sliderScreen.classList.remove("img-4");
+      sliderScreen.classList.add(`img-${slideNum}`);
+      sliderDots[slideNum - 1].classList.add("nav__dot--active");
+    }
+  }
+}, 2000);
