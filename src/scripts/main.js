133acd358f6f32423fe7fec8f6f04214ef1162d0
vkrasnovyd <v.krasnovyd@gmail.com>
2024-09-17 15:35:46 +0200

    Replace js with css in 'Tech' section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,5 @@
 'use strict';
 
-function showHideTechInfo(btn, elementId) {
-  const element = document.getElementById(elementId);
-
-  const elementStyle = window.getComputedStyle(element);
-
-  if (elementStyle.display == "none") {
-    btn.classList.replace("screen-4__info-btn--plus", "screen-4__info-btn--minus");
-    element.style.display = "block";
-  } else {
-    btn.classList.replace("screen-4__info-btn--minus", "screen-4__info-btn--plus");
-    element.style.display = "none";
-  }
-}
-
 function overlayOn(elementId) {
   document.getElementById(elementId).style.display = "grid";
   document.documentElement.style.overflow = 'hidden';
