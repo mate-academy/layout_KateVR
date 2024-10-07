3e664dc900f9293c932a1667f5bb57d16945317e
vkrasnovyd <v.krasnovyd@gmail.com>
2024-10-07 15:35:05 +0200

    Refactor main.js: apply linter, add comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,20 +3,28 @@
 const page = document.documentElement;
 
 const overlayVideoOpen = document.querySelector('.start-video__button');
-const overlayVideoClose = document.querySelector('.video-overlay__button-close');
+const overlayVideoClose = document.querySelector(
+  '.video-overlay__button-close',
+);
 const overlayVideo = document.querySelector('.video-overlay');
 
+/**
+ * Make overlay with video visible, block scrolling the page under the overlay.
+ */
 overlayVideoOpen.addEventListener('click', () => {
-  overlayVideo.style.display = "grid";
+  overlayVideo.style.display = 'grid';
   page.style.overflow = 'hidden';
-  page.scroll = "no";
-})
+  page.scroll = 'no';
+});
 
+/**
+ * Stop video and close overlay.
+ */
 overlayVideoClose.addEventListener('click', () => {
-  overlayVideo.style.display = "none";
+  overlayVideo.style.display = 'none';
   page.style.overflow = 'scroll';
-  page.scroll = "yes";
-})
+  page.scroll = 'yes';
+});
 
 const dropdown = document.querySelector('.dropdown');
 const dropdownTrigger = dropdown.querySelector('.dropdown__trigger');
